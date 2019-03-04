672eaefa3a01294c4522e30e548cad6c2f114460
Deniz Vahaboglu <deniz.vahaboglu@klm.com>
2019-03-04 09:45:08 +0100

    name change

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'vacchange-calendar'`, () => {
+  it(`should have as title 'angular-calendar'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('vacchange-calendar');
+    expect(app.title).toEqual('angular-calendar');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to vacchange-calendar!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-calendar!');
   });
 });
